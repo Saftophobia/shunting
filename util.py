0f3df05cd0d40df5a1972b1c6d23eb8faf8d0c68
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-20 15:31:40 +0200

    drawing images for testing in util.py

--- util.py
+++ util.py
@@ -1,1 +1,17 @@
 __author__ = 'saftophobia'
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def draw_image(matrix):
+    plt.matshow(matrix, fignum=100, cmap=plt.cm.gray)
+    plt.show()
+
+def draw_image_from_list(list):
+    side = int(np.sqrt(list.size))
+    draw_image(np.reshape(list, (side, side)))
+
+def shuffle_data(list):
+    np.random.shuffle(list)
+
+
