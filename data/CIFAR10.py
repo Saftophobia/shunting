d12ca1262775ab620abb752e1b3511cc2f0132a8
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-20 15:30:55 +0200

    added a case class for cifar10

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -4,10 +4,19 @@ import os, cPickle, logging
 import numpy as np
 
 class CIFAR10:
-    def __init__(self, dir = "cifar-10-batches-py"):
-        self.train  = []
-        self.test   = []
 
-        
+    def __init__(self, data_batches_count, dir = "/data/cifar-10-batches-py"):
+        logging.info("Loading %d data batches ..." % data_batches_count)
+        self.data   = []
+        self.labels = []
 
+        for i in range(1, data_batches_count + 1):
+            f = open(os.getcwd() + dir + "/data_batch_%d" %i, 'rb') #singlebatch
+            dict = cPickle.load(f)
+            f.close()
+            self.data.extend(dict["data"]) #singledim(10k,3072)
+            self.labels.extend(dict["labels"]) #singledim(1,10k)
+
+        self.data   = np.array(self.data)
+        self.labels = np.array(self.labels)
 
