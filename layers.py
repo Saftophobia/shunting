e080fa3858cd46037e2275c8b6140bd43a26616d
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-21 13:24:58 +0200

    structure for layers

---
+++ layers.py
@@ -0,0 +1,15 @@
+__author__ = 'saftophobia'
+
+class Convolutional(object):
+    def __init__(self):
+        self.filter_shape = 0
+        self.
+
+
+class Pooling(object):
+    def __init__(self):
+        self.size = 0
+
+class FullyConnected(object):
+    def __init__(self):
+        self.activation_fn = "ReLU"
