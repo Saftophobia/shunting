0f0dbd28cd57a3391cf4d169224ce170912afaaa
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-21 13:25:26 +0200

    Labels notations + tests

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 __author__ = 'saftophobia'
 
+from network import *
 from data.CIFAR10 import CIFAR10
+from util import util, logger
 
-data = CIFAR10()
+LABELS = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+dataset = CIFAR10(5)
+util.shuffle_data(dataset)
+
+
+print LABELS[dataset.labels[1]]
+util.draw_image(dataset.data[1])
+
+convnn = ConvolutionalNeuralNetworks(reg_lambda = 0.1, learning_rate = 0.01)
+
+convnn.convolute(dataset.data, 32)
