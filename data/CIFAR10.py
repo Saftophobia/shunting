5b9ccdd43a12d4729ce55caee508b63d87b9848f
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-22 14:47:33 +0200

    moved shuffle and reshapes to dataset Class

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -2,8 +2,10 @@ __author__ = 'saftophobia'
 
 import os, cPickle, logging
 import numpy as np
+from time import time
 
 class CIFAR10:
+    TAGS = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
     def __init__(self, data_batches_count, dir = "/data/cifar-10-batches-py"):
         logging.info("Loading %d data batches" % data_batches_count)
@@ -24,6 +26,22 @@ class CIFAR10:
         self.data   = np.array(self.data)
         self.labels = np.array(self.labels)
 
+        self.shuffle_data(self.data, self.labels)
+
+        # reshape array to a nxn matrix
+        img_dim = int(np.sqrt(self.data.shape[2]))
+        self.data = self.data.reshape(self.data.shape[0], self.data.shape[1], img_dim, img_dim)
+
+
     def array_to_3RGB(self, array):
         return np.array(array).reshape((3,1024))
+
+    def shuffle_data(self, data, labels):
+        seed = int(time())
+        logging.info("shuffling data with seed: %d" % seed)
+
+        r = np.random.RandomState(seed)
+        r.shuffle(data)
+        r = np.random.RandomState(seed)
+        r.shuffle(labels)
 
--- util/util.py
+++ util/util.py
@@ -1,22 +1,12 @@
 __author__ = 'saftophobia'
 
 import matplotlib.pyplot as plt
-import numpy as np
-import time, logging
+import logging
 
 def draw_image(arr):
-    reshaped = np.reshape(arr, (3, 32, 32))
-    plt.imshow(reshaped.transpose((1,2,0)))
+    #plt.imshow(arr.transpose((1,2,0)))
+    plt.imshow(arr)
     plt.show()
-
-def shuffle_data(dataset):
-    seed = int(time.time())
-    logging.info("shuffling data with seed: %d" % seed)
-
-    r = np.random.RandomState(seed)
-    r.shuffle(dataset.data)
-    r = np.random.RandomState(seed)
-    r.shuffle(dataset.labels)
 
 
 
