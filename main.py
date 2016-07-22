ab679acf10142ac9aa4bb473066538aae654d8ed
Ahmed Elsafty <ahmed.elsafty@xing.com>
2016-07-22 14:49:46 +0200

    calling the examples from network.py

--- main.py
+++ main.py
@@ -4,15 +4,28 @@ from network import *
 from data.CIFAR10 import CIFAR10
 from util import util, logger
 
-LABELS = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
 dataset = CIFAR10(5)
-util.shuffle_data(dataset)
+
+#print dataset.TAGS[dataset.labels[1]]
+#util.draw_image(dataset.data[1])
 
 
-print LABELS[dataset.labels[1]]
-util.draw_image(dataset.data[1])
+v = ConvolutionalNeuralNetworks(layers=[
+        Convolutional(num_of_output_featureMaps = 16,
+                       prev_layer_stack_size = 3,
+                       filter_size = 5,
+                       input_size = 32,
+                       mini_batch_size= 64)
+        #Pooling()
+                        ])
+v.train(dataset.data[1:15], dataset.labels[1:15])
 
-convnn = ConvolutionalNeuralNetworks(reg_lambda = 0.1, learning_rate = 0.01)
+#num_of_imgs (usually batch size), prev_layer_num_of_pooled_input, img_width, img_height
 
-convnn.convolute(dataset.data, 32)
+mini_batch_size=4
+num_prev_layer_channels = 3
+img_width = 32
+img_height = 32
+
+
+#conv2d.forward(input=dataset.data[0:4])
